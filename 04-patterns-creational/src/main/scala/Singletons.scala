8ab6365994c5c1a517c8ad836b8fbacadf8f9156
Piotr Krzeminski <pio.krzeminski@gmail.com>
2014-03-23 17:38:00 +0100

    per-thread instance singleton + UT

--- 04-patterns-creational/src/main/scala/Singletons.scala
+++ 04-patterns-creational/src/main/scala/Singletons.scala
@@ -14,4 +14,19 @@ object Singletons {
     }
   }
 
+  class PerThreadInstanceSingleton[T](create: => T) extends Singleton[T] {
+    private var instances = Map.empty[Long, T]
+    def get: T = {
+      val threadId = Thread.currentThread().getId
+      println(s"threadID: $threadId")
+      instances.get(threadId) match {
+        case Some(instance) => instance
+        case None =>
+          val inst = create
+          instances += (threadId -> inst)
+          inst
+      }
+    }
+  }
+
 }
--- 04-patterns-creational/src/test/scala/SingletonsSpec.scala
+++ 04-patterns-creational/src/test/scala/SingletonsSpec.scala
@@ -2,17 +2,50 @@ import org.specs2.mutable._
 import Singletons._
 
 class SingletonsSpec extends Specification {
+
+  
   "SingleInstanceSingleton" should {
     "create an instance" in {
-      val singleton = new SingleInstanceSingleton(new String("abc"))
+      val singleton = new SingleInstanceSingleton(Some("abc"))
       singleton.get must not beNull
     }
 
     "create an unique instance" in {
-      val singleton = new SingleInstanceSingleton(new String("abc"))
+      val singleton = new SingleInstanceSingleton(Some("abc"))
       val obj1 = singleton.get
       val obj2 = singleton.get
       (obj1 eq obj2) must beTrue
+    }
+  }
+
+  "PerThreadInstanceSingleton" should {
+    "create an instance" in {
+      val singleton = new PerThreadInstanceSingleton(Some("abc"))
+      singleton.get must not beNull
+    }
+
+    "create an unique instance in the same thread" in {
+      val singleton = new PerThreadInstanceSingleton(Some("abc"))
+      val obj1 = singleton.get
+      val obj2 = singleton.get
+      (obj1 eq obj2) must beTrue
+    }
+
+    "create separate instances in different threads" in {
+      val singleton = new PerThreadInstanceSingleton(Some("abc"))
+      var obj1: Object = null
+      var obj2: Object = null
+      val t1: Thread = new Thread(new Runnable {
+        override def run(): Unit = { obj1 = singleton.get }
+      })
+      val t2: Thread = new Thread(new Runnable {
+        override def run(): Unit = { obj2 = singleton.get }
+      })
+      t1.start()
+      t2.start()
+      t1.join()
+      t2.join()
+      (obj1 eq obj2) must beFalse
     }
   }
 
