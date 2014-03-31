0f0efd1dfdfbabf3ece96c287a916eb7191bbf37
Piotr Krzemiński <pio.krzeminski@gmail.com>
2014-03-31 15:55:43 +0200

    ex. 2 (WIP)

--- 05-patterns-structural/src/main/scala/Decorator.scala
+++ 05-patterns-structural/src/main/scala/Decorator.scala
@@ -1,14 +1,32 @@
 
-import java.io.{InputStream, OutputStream}
+import java.io._
 
 object Decorator extends App {
 
   class CaesarInputStream(is: InputStream, shift: Int) extends InputStream {
-    override def read(): Int = is.read + shift
+    override def read(): Int = shift + is.read()
   }
 
-  //class CaesarOutputStream
+  class CaesarOutputStream(os: OutputStream, shift: Int) extends OutputStream {
+    override def write(c: Int): Unit = os.write(shift + c)
+  }
 
+  val bais = new ByteArrayInputStream("abc".getBytes)
+  val cis = new CaesarInputStream(bais, 5)
+
+  //val bis = new BufferedInputStream(cis)
+  //val isr = new InputStreamReader(bis)
+  //val br = new BufferedReader(isr)
+  //println(br.readLine())
+
+
+
+
+  val sw = new StringWriter
+  sw.write("abc")
+  //sw
+
+  println(sw.getBuffer.toString)
 
 
 
