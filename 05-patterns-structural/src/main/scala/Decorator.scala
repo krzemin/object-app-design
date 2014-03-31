b5b04acbffa2be9ab6a7bdb7ccbebe5e3b94baad
Piotr Krzeminski <pio.krzeminski@gmail.com>
2014-03-31 16:59:28 +0200

    decorator finished

--- 05-patterns-structural/src/main/scala/Decorator.scala
+++ 05-patterns-structural/src/main/scala/Decorator.scala
@@ -4,30 +4,29 @@ import java.io._
 object Decorator extends App {
 
   class CaesarInputStream(is: InputStream, shift: Int) extends InputStream {
-    override def read(): Int = shift + is.read()
+    override def read(): Int = {
+      val b = is.read()
+      if (b == -1) b else b + shift
+    }
   }
 
   class CaesarOutputStream(os: OutputStream, shift: Int) extends OutputStream {
     override def write(c: Int): Unit = os.write(shift + c)
   }
 
-  val bais = new ByteArrayInputStream("abc".getBytes)
-  val cis = new CaesarInputStream(bais, 5)
+  val origText = "abc"
+  println(s"original text is $origText")
 
-  //val bis = new BufferedInputStream(cis)
-  //val isr = new InputStreamReader(bis)
-  //val br = new BufferedReader(isr)
-  //println(br.readLine())
+  val baos = new ByteArrayOutputStream(10)
+  val cos = new CaesarOutputStream(baos, 5)
+  cos.write(origText.getBytes)
 
+  println(baos.toString)
 
+  val bais = new ByteArrayInputStream(baos.toByteArray)
+  val cis = new CaesarInputStream(bais, -5)
+  val isr = new InputStreamReader(cis)
+  val br = new BufferedReader(isr)
 
-
-  val sw = new StringWriter
-  sw.write("abc")
-  //sw
-
-  println(sw.getBuffer.toString)
-
-
-
+  println(br.readLine())
 }
