f9a0d648af87c4393f2ec7d49a189508df6eabae
Piotr Krzemiński <pio.krzeminski@gmail.com>
2014-04-13 17:03:20 +0200

    interpreter finished

--- 06-patterns-behavioral1/src/test/scala/InterpreterSpec.scala
+++ 06-patterns-behavioral1/src/test/scala/InterpreterSpec.scala
@@ -15,9 +15,10 @@ class InterpreterSpec extends Specification {
       new Neg(False).eval(ctx) must beTrue
     }
     "evaluate variables" in {
-      val ctx1: Context = ctx + ("x" -> true)
+      val ctx1: Context = ctx + ("x" -> true, "y" -> false)
       new Var("x").eval(ctx1) must beTrue
-      new Var("y").eval(ctx1) must throwA[RuntimeException]
+      new Var("y").eval(ctx1) must beFalse
+      new Var("z").eval(ctx1) must throwA[RuntimeException]
     }
     "evaluate conjunction" in {
       new And(True, True).eval(ctx) must beTrue
@@ -28,8 +29,13 @@ class InterpreterSpec extends Specification {
     "evaluate disjunction" in {
       new Or(True, True).eval(ctx) must beTrue
       new Or(False, False).eval(ctx) must beFalse
-      new Or(False, True).eval(ctx) must beFalse
+      new Or(False, True).eval(ctx) must beTrue
       new Or(True, new Var("y")).eval(ctx) must beTrue
+    }
+    "evaluate complex expressions" in {
+      val ctx1: Context = ctx + ("x" -> false, "y" -> true)
+      val e1 = new And(new Var("y"), new Or(new Neg(new Var("y")), True))
+      e1.eval(ctx1) must beTrue
     }
   }
 
